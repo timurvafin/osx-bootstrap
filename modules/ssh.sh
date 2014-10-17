6e625eb580f1b27486bff01f9afb8b1bfb55fe0f
Timur Vafin <timur.vafin@flatstack.com>
2014-10-17 21:26:46 +0400

    Do not run ssh

--- modules/ssh.sh
+++ modules/ssh.sh
@@ -1,5 +1,5 @@
 info_echo "Checking for SSH key, generating one if it doesn't exist"
-[[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa
+[[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa -f ~/.ssh/id_rsa.pub
 
 info_echo "Copying public key to clipboard. Paste it into your Github account"
 [[ -f ~/.ssh/id_rsa.pub ]] && cat ~/.ssh/id_rsa.pub | pbcopy
